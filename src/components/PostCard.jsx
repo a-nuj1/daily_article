13a00c17a5871cebad32456883e444f4b55fc2b8
Anuj <anujgupta1532003@gmail.com>
2024-05-14 23:00:35 +0530

    tail

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -4,7 +4,8 @@ import {Link} from "react-router-dom"
 
 function PostCard({$id, title , featuredImage}) {
   return (
-    <div>PostCard</div>
+    //checking error in git
+    <Link to={``}/>
   )
 }
 
