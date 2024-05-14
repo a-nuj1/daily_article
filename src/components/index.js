2e75b1feab8af9be6252ca03f5002aaaaec588f5
Anuj <anujgupta1532003@gmail.com>
2024-05-14 20:28:11 +0530

    added select file

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Logo from "./logo";
+import Container from "./Container/Container"
+import LogoutBtn from './Header/LogoutBtn'
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
+}
