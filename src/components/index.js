f1909c140dc0efeac2eace834ec05e54a640731f
Anuj <anujgupta1532003@gmail.com>
2024-05-16 21:06:43 +0530

    ready post-form

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Footer from "./Footer/Footer";
 import Logo from "./logo";
 import Container from "./Container/Container"
 import LogoutBtn from './Header/LogoutBtn'
+import RTE from "./RTE";
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE
 }
